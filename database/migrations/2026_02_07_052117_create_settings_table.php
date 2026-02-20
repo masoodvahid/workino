76a6d1c96b91dd2a69c8433a3cebf588e8b09ef8
Masood <masood.vahid@gmail.com>
2026-02-21 00:38:34 +0330

    0.9

--- database/migrations/2026_02_07_052117_create_settings_table.php
+++ database/migrations/2026_02_07_052117_create_settings_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();            
-            $table->string('key', 128);
+            $table->string('key', 100);
             $table->json('value'); 
-            $table->string('group', 128)->nullable();  // ex: user_profile, sms, ...
+            $table->string('group', 100)->nullable();  // ex: user_profile, sms, ...
             $table->softDeletes();
             $table->timestamps();
 
